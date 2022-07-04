b045584ac60bb86b99630965e6ca16b62c5f80ab
iLynette <lynetteacholah@icloud.com>
2022-07-04 20:41:43 +0300

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :categories, dependent: :destroy
     has_many :payments, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 250 }
 end
