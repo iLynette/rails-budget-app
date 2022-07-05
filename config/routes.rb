78206b5f0eb13366492fb1e1ecac1df7335537cc
iLynette <lynetteacholah@icloud.com>
2022-07-05 05:51:30 +0300

    Devise in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :payments
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  # root "home#index"
 end
