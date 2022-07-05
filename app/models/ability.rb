1c0c110feb331061088bc276fd9e0f3191a20eae
iLynette <lynetteacholah@icloud.com>
2022-07-05 07:49:11 +0300

    Add payment in ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,7 @@ class Ability
       can :manage, :all
     else
       can :manage, Category, user_id: user.id
-      can :manage, Category, user_id: user.id
+      can :manage, Payment, user_id: user.id
     end
     #   return unless user.present?
     #   can :read, :all
