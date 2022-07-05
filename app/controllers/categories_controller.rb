c616d87ee0f3143475406a75659c7557929c57e7
iLynette <lynetteacholah@icloud.com>
2022-07-05 07:13:01 +0300

    Define root index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,12 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
 
-  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
+    @payments = @category.payments.order('created_at DESC')
   end
 
   # GET /categories/new
