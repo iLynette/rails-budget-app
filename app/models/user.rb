32c5cb168bf005ddbef226bfd4d63d1b5bda0960
iLynette <lynetteacholah@icloud.com>
2022-07-06 18:51:37 +0300

    Add author id as foreign key in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :categories, dependent: :destroy
-    has_many :payments, dependent: :destroy
+    has_many :categories, dependent: :destroy, foreign_key: 'author_id'
+    has_many :payments, dependent: :destroy, foreign_key: 'author_id'
 
     validates :name, presence: true, length: { maximum: 250 }
 
