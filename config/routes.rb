fa5888447745feb8e214d168fa82701a82a756f8
iLynette <lynetteacholah@icloud.com>
2022-07-08 12:29:18 +0300

    Authenticated & non authenticated users to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
-  root 'categories#index'
+  unauthenticated do
+     root 'users#index'
+    end
+
+    authenticated do
+      root 'categories#index', as: :authenticated_root
+    end
 end
