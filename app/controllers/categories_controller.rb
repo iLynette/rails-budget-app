a4b070a2f3b2e3977282677c4ad0e599b6da6396
iLynette <lynetteacholah@icloud.com>
2022-08-16 22:48:53 +0300

    Clean code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,15 +8,10 @@ class CategoriesController < ApplicationController
     @payments = @category.payments.order(created_at: :desc)
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit; end
-
-  # POST /categories or /categories.json
   def create
     if current_user.nil?
       redirect_to categories_path
@@ -36,7 +31,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
   def update
     respond_to do |format|
       if @category.update(category_params)
@@ -49,7 +43,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy
 
@@ -60,8 +53,6 @@ class CategoriesController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
 
   # Only allow a list of trusted parameters through.
   def category_params
