2aceb9f6bad9def0f3d21999740e26c038531c50
iLynette <lynetteacholah@icloud.com>
2023-01-18 18:04:05 +0300

    Config hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "rails-budget-app-production-cf9c.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
